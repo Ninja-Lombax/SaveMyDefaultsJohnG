f7460425f0be15c2494340bd8cedbae7ae550378
Ninja-Lombax <godzillakotm80@gmail.com>
2020-10-07 11:36:56 -0500

    Initial Commit

---
+++ SaveMyDefaultsJohnG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SaveMyDefaultsJohnG
+//
+//  Created by John Grasser on 10/7/20.
+//  Copyright © 2020 John Grasser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
